c8b636c657e0d2f7197c266dfa65f8bc078958c0
Nicolò Spuri <nicolo.spuri@mail.polimi.it>
2025-08-03 16:26:30 +0200

    aggiustato problemi con il db

--- Progetto-TIW-2025-RIA/src/test/java/ConnectionTester.java
+++ Progetto-TIW-2025-RIA/src/test/java/ConnectionTester.java
@@ -3,12 +3,12 @@ public class ConnectionTester {
     public static void main(String[] args) throws SQLException,
             ClassNotFoundException {
         final String DATABASE = "Progetto_Web";
-        final String USER = "nicolospuri";
-        final String PASSWORD = "nicky2003";
+        final String USER = "progetto_web";
+        final String PASSWORD = "progetto_web";
         Connection connection = null;
 // Load the JDBC driver
         try {
-            Class.forName("org.mariadb.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded");
         } catch (ClassNotFoundException e) {
             System.out.println("Driver not found");
@@ -16,7 +16,7 @@ public class ConnectionTester {
         }
         try {
             connection = DriverManager.getConnection
-                    ("jdbc:mariadb://localhost:3306/" + DATABASE, USER, PASSWORD);
+                    ("jdbc:mysql://localhost:3306/" + DATABASE + "?serverTimezone=UTC", USER, PASSWORD);
             System.out.println("Database connected");
             connection.close();
         } catch (Exception e) {
